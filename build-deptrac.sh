203e979d35b5a36e95d196c7d4157a144c26b04a
Gennadi McKelvey <gennadijanzen@icloud.com>
2023-11-23 22:11:56 +0100

    cleanup build script
    update build script
    
    update build script

--- build-deptrac.sh
+++ build-deptrac.sh
@@ -1,16 +1,47 @@
-rm -rf deptrac-build
-composer install -a --no-dev
+#!/usr/bin/env bash
 
-php -dmemory_limit=-1 build/php-scoper.phar add-prefix \
-    -c scoper.inc.php --working-dir . \
-    config internal src vendor deptrac deptrac.config.php deptrac.php
+DEPTRAC_DIR="${DEPTRAC_DIR:-../deptrac}"
+BUILD_DIR=build
+BUILD_TMP=${BUILD_DIR}/tmp
+PHP='docker compose exec -u 1000 php php -d memory_limit=-1'
+SCOPER=$BUILD_DIR/php-scoper.phar
 
-cp composer.* deptrac-build/
-cp deptrac.yaml deptrac-build/
+info()
+{
+    MESSAGE=$1;
 
-cd deptrac-build && composer dump -a --dev
+    printf "\n";
+    echo "######### $MESSAGE ########";
+    printf "\n";
+}
 
-echo 'RUN self test'
-./deptrac
+info "Start build deptrac"
 
-cd .. && composer install -a --dev
+info "Install composer"
+$PHP /usr/bin/composer install -a --no-dev
+
+info "Scope deptrac"
+$PHP $SCOPER add-prefix --force --config scoper.inc.php --working-dir . --output-dir $BUILD_TMP
+
+info "Dump Composer Autoloader"
+$PHP /usr/bin/composer dump-autoload --working-dir $BUILD_TMP -a --no-dev
+
+info "Copy package templates"
+cp -v -R $BUILD_DIR/template/* *.md mkdocs.yml .github docs -t $BUILD_TMP
+
+info "Copy build into deptrac distrubtion repository"
+cp -v -a $BUILD_TMP/. $DEPTRAC_DIR
+
+info "Git commit changes"
+echo "Update $(date)" > git_commit_message.txt
+echo "" >> git_commit_message.txt
+git log $(git describe --tags --abbre=0)..HEAD --oneline >> git_commit_message.txt
+
+git -C $DEPTRAC_DIR add .
+mv git_commit_message.txt $DEPTRAC_DIR
+git -C $DEPTRAC_DIR commit  -F git_commit_message.txt
+git -C $DEPTRAC_DIR reset --hard
+git -C $DEPTRAC_DIR clean -fd
+
+info "Build done!"
+
--- scoper.inc.php
+++ scoper.inc.php
@@ -2,18 +2,34 @@
 
 declare(strict_types=1);
 
-use Isolated\Symfony\Component\Finder\Finder as IsolatedFinder;
+use Isolated\Symfony\Component\Finder\Finder;
 
-$polyfillsBootstrap = IsolatedFinder::create()
+$datePrefix = (new DateTimeImmutable('now'))->format('Ym');
+$polyfillsBootstrap = Finder::create()
     ->files()
     ->in(__DIR__.'/vendor/symfony/polyfill-*')
     ->name('*.php');
 
 return [
-    'prefix' => 'DEPTRAC_'.time(),
-    'finders' => [],
+    'prefix' => 'DEPTRAC_'.$datePrefix,
+    'finders' => [
+        Finder::create()->files()->in([
+            'config',
+            'internal',
+            'src',
+            'vendor',
+        ])->append([
+            'deptrac',
+            'deptrac.config.php',
+            'deptrac.php',
+            'composer.json',
+        ])->exclude([
+            'bin',
+            'tests',
+            'test',
+        ])->notName('/.*\\.(xml|md|dist|neon)|Makefile|composer\\.json|composer\\.lock/'),
+    ],
     'patchers' => [],
-    'output-dir' => './deptrac-build/',
     'tag-declarations-as-internal' => false,
     'exclude-files' => array_map(
         static function ($file) {
@@ -26,7 +42,7 @@ return [
         'Symfony\Polyfill',
     ],
     'expose-functions' => ['trigger_deprecation'],
-    'expose-global-constants' => false,
-    'expose-global-classes' => false,
-    'expose-global-functions' => false,
+    'expose-global-constants' => true,
+    'expose-global-classes' => true,
+    'expose-global-functions' => true,
 ];
